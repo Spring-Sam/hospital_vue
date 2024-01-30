1a38311861969a4b19d6c7355f77d5f1626da637
Sam <552352258@qq.com>
2024-01-30 17:54:14 +0800

    feat(B/E): 初始化後端項目

---
+++ xm_hospital_manager/springboot/src/main/java/com/example/controller/DoctorController.java
@@ -0,0 +1,87 @@
+package com.example.controller;
+
+import com.example.common.Result;
+import com.example.entity.Doctor;
+import com.example.service.DoctorService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 管理员前端操作接口
+ **/
+@RestController
+@RequestMapping("/doctor")
+public class DoctorController {
+
+    @Resource
+    private DoctorService doctorService;
+
+    /**
+     * 新增
+     */
+    @PostMapping("/add")
+    public Result add(@RequestBody Doctor Doctor) {
+        doctorService.add(Doctor);
+        return Result.success();
+    }
+
+    /**
+     * 删除
+     */
+    @DeleteMapping("/delete/{id}")
+    public Result deleteById(@PathVariable Integer id) {
+        doctorService.deleteById(id);
+        return Result.success();
+    }
+
+    /**
+     * 批量删除
+     */
+    @DeleteMapping("/delete/batch")
+    public Result deleteBatch(@RequestBody List<Integer> ids) {
+        doctorService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    /**
+     * 修改
+     */
+    @PutMapping("/update")
+    public Result updateById(@RequestBody Doctor Doctor) {
+        doctorService.updateById(Doctor);
+        return Result.success();
+    }
+
+    /**
+     * 根据ID查询
+     */
+    @GetMapping("/selectById/{id}")
+    public Result selectById(@PathVariable Integer id) {
+        Doctor doctor = doctorService.selectById(id);
+        return Result.success(doctor);
+    }
+
+    /**
+     * 查询所有
+     */
+    @GetMapping("/selectAll")
+    public Result selectAll(Doctor Doctor ) {
+        List<Doctor> list = doctorService.selectAll(Doctor);
+        return Result.success(list);
+    }
+
+    /**
+     * 分页查询
+     */
+    @GetMapping("/selectPage")
+    public Result selectPage(Doctor Doctor,
+                             @RequestParam(defaultValue = "1") Integer pageNum,
+                             @RequestParam(defaultValue = "10") Integer pageSize) {
+        PageInfo<Doctor> page = doctorService.selectPage(Doctor, pageNum, pageSize);
+        return Result.success(page);
+    }
+
+}
